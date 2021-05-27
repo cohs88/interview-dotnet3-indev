47cd840ed34a94d1ebac371061da426a78f48e1e
servando coria <servando.coria@uabc.edu.mx>
2021-05-27 12:41:10 -0700

    swagger added

--- GroceryStoreAPI/Startup.cs
+++ GroceryStoreAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace GroceryStoreAPI
 {
@@ -19,6 +20,21 @@ namespace GroceryStoreAPI
 
         public IConfiguration Configuration { get; }
 
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"Customers {groupName}",
+                    Version = groupName,
+                    Description = "Customers API"
+                });
+            });
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -28,6 +44,7 @@ namespace GroceryStoreAPI
             services.AddTransient<ICustomerService, CustomerService>();
 
             services.AddControllers();
+            AddSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +54,12 @@ namespace GroceryStoreAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customers API V1");
+            });
 
             app.UseRouting();
 
